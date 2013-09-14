7eede475abb55cf49901ffeb4c66df34a4c5b286
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-09-14 11:31:20 -0700

    .envrc + rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfuncubeHackathon::Application.config.secret_token = '6bb815db0956fc762133c06a887d4dd028748f8c63996d910acd4d53e3023593d7da142338a58e86a516486c5a3d3e3ad441b322fd4030dcc94c91c730e6c35d'
