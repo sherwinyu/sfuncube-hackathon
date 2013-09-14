198e3079dc6f6caa5fdd5a102e547164fd0a42dd
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-09-14 11:37:10 -0700

    install and bunddle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require sfuncube_hackathon
+
+// for more details see: http://emberjs.com/guides/application/
+SfuncubeHackathon = Ember.Application.create();
+
 //= require_tree .
